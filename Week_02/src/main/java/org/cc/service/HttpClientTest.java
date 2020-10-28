6f9289b274ee54f81ba741b70b60592202bddccd
liuthomas725 <liuthomas725@gmail.com>
2020-10-28 21:12:49 +0800

    提交作业2

---
+++ Week_02/src/main/java/org/cc/service/HttpClientTest.java
@@ -0,0 +1,34 @@
+package org.cc.service;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClientTest {
+
+    public static void main(String[] args) {
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(5000)
+                .setConnectionRequestTimeout(5000)
+                .setSocketTimeout(5000)
+                .setRedirectsEnabled(true).build();
+        httpGet.setConfig(requestConfig);
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build(); CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
